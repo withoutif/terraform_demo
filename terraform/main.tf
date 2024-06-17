5767166255ab30681f2151820d92ecf5eda7aaf9
Tak <tkmogel@gmail.com>
2024-06-17 17:45:04 -0400

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+terraform {
+   backend "s3" {
+       bucket = "withoutif-terraform-demo"
+       key    = "terraform.tfstate"
+       region = "us-east-1"
+   }
+}
+
+#resource "aws_instance" "test-instance" {
+#  ami           = "ami-04b70fa74e45c3917"
+#  instance_type = "t2.micro"
+#  tags = {
+#    Name = "test-instance"
+#  }
+}
