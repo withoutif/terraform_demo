9e746b6ddec6b8d051ae9ead27b454e5c13e0ae6
Tak <tkmogel@gmail.com>
2024-06-17 18:28:23 -0400

    restore instance. test run: cmd with apply

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,10 +10,10 @@ terraform {
    }
 }
 
-#resource "aws_instance" "test-instance" {
-#  ami           = "ami-04b70fa74e45c3917"
-#  instance_type = "t2.micro"
-#  tags = {
-#    Name = "test-instance"
-#  }
-# }
+resource "aws_instance" "test-instance" {
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "test-instance"
+  }
+}
